03d65226270f7f8d3e196cbd6f45305c006bffb1
Rakshit Ameriya <rakshitameriya101@gmail.com>
2022-11-16 19:33:06 +0530

    removed unnecessary connectors

--- connectors/index.ts
+++ connectors/index.ts
@@ -4,57 +4,17 @@ import {
   NoEthereumProviderError,
   UserRejectedRequestError as UserRejectedRequestErrorInjected,
 } from "@web3-react/injected-connector";
-import { UserRejectedRequestError as UserRejectedRequestErrorWalletConnect } from "@web3-react/walletconnect-connector";
-import { UserRejectedRequestError as UserRejectedRequestErrorFrame } from "@web3-react/frame-connector";
 
-import {
-  authereum,
-  fortmatic,
-  frame,
-  injected,
-  lattice,
-  ledger,
-  magic,
-  network,
-  portis,
-  torus,
-  trezor,
-  walletconnect,
-  walletlink,
-} from "connectors/connectors";
+import { injected } from "connectors/connectors";
 
 export enum ConnectorNames {
   Injected = "Injected",
-  Network = "Network",
-  WalletConnect = "WalletConnect",
-  WalletLink = "WalletLink",
-  Ledger = "Ledger",
-  Trezor = "Trezor",
-  Lattice = "Lattice",
-  Frame = "Frame",
-  Authereum = "Authereum",
-  Fortmatic = "Fortmatic",
-  Magic = "Magic",
-  Portis = "Portis",
-  Torus = "Torus",
 }
 
 export type ConnectorType = ConnectorNames | null;
 
 export const connectorsByName: { [connectorName in ConnectorNames]: any } = {
   [ConnectorNames.Injected]: injected,
-  [ConnectorNames.Network]: network,
-  [ConnectorNames.WalletConnect]: walletconnect,
-  [ConnectorNames.WalletLink]: walletlink,
-  [ConnectorNames.Ledger]: ledger,
-  [ConnectorNames.Trezor]: trezor,
-  [ConnectorNames.Lattice]: lattice,
-  [ConnectorNames.Frame]: frame,
-  [ConnectorNames.Authereum]: authereum,
-  [ConnectorNames.Fortmatic]: fortmatic,
-  [ConnectorNames.Magic]: magic,
-  [ConnectorNames.Portis]: portis,
-  [ConnectorNames.Torus]: torus,
 };
 
 export function getErrorMessage(error: Error) {
@@ -62,11 +22,7 @@ export function getErrorMessage(error: Error) {
     return "No Ethereum browser extension detected, install MetaMask on desktop or visit from a dApp browser on mobile.";
   } else if (error instanceof UnsupportedChainIdError) {
     return "You're connected to an unsupported network.";
-  } else if (
-    error instanceof UserRejectedRequestErrorInjected ||
-    error instanceof UserRejectedRequestErrorWalletConnect ||
-    error instanceof UserRejectedRequestErrorFrame
-  ) {
+  } else if (error instanceof UserRejectedRequestErrorInjected) {
     return "Please authorize this website to access your Ethereum account.";
   } else {
     console.error(error);
