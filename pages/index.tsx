f573a290c4288d28dbca8ff44e358e40d03e95c9
Rakshit Ameriya <rakshitameriya101@gmail.com>
2022-11-16 10:11:59 +0530

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="p-2">
+      <Head>
+        <title>Neptune Mutual</title>
+        <meta name="description" content="React test excercise" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}
